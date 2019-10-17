b5904f9949c11a1cb68ad6ccf7bf07344db285f6
aburkland <abbyburkland@gmail.com>
2019-10-17 16:18:00 -0400

    Added HTML and started to build out sign-up steps
    Working on jQuery and building out steps for sign-up Ajax call to Api

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 let apiUrl
 const apiUrls = {
   production: 'https://desolate-badlands-44499.herokuapp.com',
-  development: 'http://localhost:4741'
+  development: 'http://localhost:4741' // client facing server uses - 7165
 }
 
 if (window.location.hostname === 'localhost') {
